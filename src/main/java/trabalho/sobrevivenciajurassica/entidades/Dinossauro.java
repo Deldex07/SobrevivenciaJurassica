df1fa6ce95552cfcc73cc9c164488f4b7e33f37a
Felipe Lima <felipelr2007@gmail.com>
2026-06-02 13:02:25 -0300

    Estrutura inicial de pacotes, superclasses e interfaces

---
+++ src/main/java/trabalho/sobrevivenciajurassica/entidades/Dinossauro.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabalho.sobrevivenciajurassica.entidades;
+
+import trabalho.sobrevivenciajurassica.interfaces.Atacante;
+
+/**
+ *
+ * @author deldex
+ */
+public abstract class Dinossauro extends EntidadeViva implements Atacante {
+    
+}
