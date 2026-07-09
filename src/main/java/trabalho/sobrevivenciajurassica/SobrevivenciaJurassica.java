835772e48494566ae8d88ceff5f96ee54dd9db3b
Felipe Lima <felipelr2007@gmail.com>
2026-07-09 19:21:07 -0300

    feat: Atualização da main com IOException

--- src/main/java/trabalho/sobrevivenciajurassica/SobrevivenciaJurassica.java
+++ src/main/java/trabalho/sobrevivenciajurassica/SobrevivenciaJurassica.java
@@ -2,9 +2,10 @@ package trabalho.sobrevivenciajurassica;
 
 import trabalho.sobrevivenciajurassica.logica.GerenciadorJogo;
 
+import java.io.IOException;
 public class SobrevivenciaJurassica {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         GerenciadorJogo jogo = new GerenciadorJogo();
         jogo.iniciarJogo();
     }
