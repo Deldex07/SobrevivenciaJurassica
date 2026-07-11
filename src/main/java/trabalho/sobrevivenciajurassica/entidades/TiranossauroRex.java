f37f2abbf41daba2e1fcbc5c90e461b5f76553ba
Felipe Lima <felipelr2007@gmail.com>
2026-07-11 12:44:50 -0300

    feat: Atualização do Tiranossauro Rex

--- src/main/java/trabalho/sobrevivenciajurassica/entidades/TiranossauroRex.java
+++ src/main/java/trabalho/sobrevivenciajurassica/entidades/TiranossauroRex.java
@@ -3,17 +3,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package trabalho.sobrevivenciajurassica.entidades;
-import trabalho.sobrevivenciajurassica.comportamentos.MovimentoParado;
 /**
  * Representa um Tiranossauro Rex, um dinossauro grande e poderoso.
  * TiranossauroRex
  * @author deldex
  */
 public class TiranossauroRex extends Dinossauro {
-
     public TiranossauroRex(int saudeInicial, int linha, int coluna, char simbolo) {
         super(saudeInicial, linha, coluna, simbolo);
-        this.comportamentoMovimento = new MovimentoParado();
+        // comportamentoMovimento permanece null — T-Rex nunca se move
     }
 
     @Override
